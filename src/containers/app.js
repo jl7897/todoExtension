82c67d8ce6bce0f3f73277f26cecf32a40d2e2b7
Jacob Lee <jake@Jacobs-MacBook-Pro.local>
2018-03-17 10:23:33 -0500

    Added today and week lists as accordians with dropdown text box

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
+import { Divider, Icon, Accordion } from 'semantic-ui-react';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      value: '',
+      today: '',
+      week: '',
       text: '',
+      activeIndex: 0,
     };
   }
 
-  onChange = (e) => {
-    this.setState({ value: e.target.value });
+  onChangeToday = (e) => {
+    this.setState({ today: e.target.value });
   }
 
-  onEnter = (e) => {
+  onChangeWeek = (e) => {
+    this.setState({ week: e.target.value });
+  }
+
+  onEnterToday = (e) => {
     if (e.keyCode === 13) {
-      this.setState({
-        value: '',
-        text: this.state.value,
-      });
+      this.setState({ text: this.state.today });
     }
   }
 
-  render = () => (
-    <div>
-      <input value={this.state.value} placeholder='Type something' onChange={this.onChange} onKeyDown={this.onEnter}/>
-      <p>{this.state.text}</p>
-    </div>
-  );
+  onEnterWeek = (e) => {
+    if (e.keyCode === 13) {
+      this.setState({ text: this.state.week });
+    }
+  }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render = () => {
+    const { activeIndex } = this.state;
+    return (
+      <div>
+        <h3>To do's</h3>
+        <Divider/>
+        <Accordion styled>
+          <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+            <Icon name='plus square outline' />
+            Today
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 0}>
+            <input value={this.state.today} placeholder='Type something' onChange={this.onChangeToday} onKeyDown={this.onEnterToday}/>
+          </Accordion.Content>
+          <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+            <Icon name='plus square outline' />
+            Week
+          </Accordion.Title>
+          <Accordion.Content active={activeIndex === 1}>
+            <input value={this.state.week} placeholder='Type something' onChange={this.onChangeWeek} onKeyDown={this.onEnterWeek} />
+          </Accordion.Content>
+        </Accordion>
+        <p>{this.state.text}</p>
+      </div>
+    );
+  };
 }
 
 
