3eb3ccdce1a7e170713d96772960246b65ec6168
Jake Lee <jacob.lee715@gmail.com>
2018-04-18 13:13:48 -0500

    Made some small changes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Divider, Icon } from 'semantic-ui-react';
+import { Divider, Icon, Popup, Button } from 'semantic-ui-react';
 
 const ListItem = props => (
     <div className={props.item.done ? 'todoListItemDone' : 'todoListItem'}>
-      <div className='todoListItem'><Icon onClick={() => { props.toggleDone(props.index, props.time); }} name={props.item.done ? 'check' : 'circle'} color={props.item.done ? 'green' : 'yellow'} />
-      {props.item.todo}  Repeat: {props.item.repeats.map(item => `${item}, `)}
-        <span style={{ float: 'right' }}>
-          <Icon onClick={() => { props.onDelete(props.index, props.time); }} name='trash'/>
+      <div className='todoListItem'>
+      <Icon onClick={() => { props.toggleDone(props.index, props.time); }} name={props.item.done ? 'check' : 'circle'} color={props.item.done ? 'green' : 'yellow'} />
+        {props.item.todo}
+        <span style={{
+          float: 'right',
+          marginLeft: '1%',
+          }}>
+          <Popup
+            hoverable
+            trigger={<Icon name='trash' />}
+            content={<Button onClick={() => { props.onDelete(props.index, props.time); }} color='red' content='Really delete?' />}
+            on='hover'
+            position='top left'
+          />
+        </span>
+        {
+          // put this in the details body instead
+        }
+        <span style={{
+          float: 'right',
+          marginRight: '5%',
+        }}>
+          {props.item.repeats.map((item, i) => <span key={i} className='daysRepeated'>{item}</span>)}
         </span>
       </div>
       <Divider fitted />
